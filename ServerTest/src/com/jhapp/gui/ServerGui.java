c082301f26a204af26f79fa2292156374e2d9e7f
ny658 <ny658@DESKTOP-QENFOBC>
2016-11-28 23:27:28 +0900

    gui 클래스 분리

---
+++ ServerTest/src/com/jhapp/gui/ServerGui.java
@@ -0,0 +1,29 @@
+package com.jhapp.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class ServerGui {
+	
+	JFrame frame = new JFrame("ServerTest");
+	Container contentPane = frame.getContentPane();
+    JTextField text = new JTextField();
+    JButton button = new JButton("¿˙¿Â");
+	
+	public ServerGui() {
+		frame.setPreferredSize(new Dimension(500, 500));
+        frame.setLocation(500, 400);
+        
+        contentPane.add(text, BorderLayout.CENTER);
+        contentPane.add(button, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+		
+	}
+}
--- ServerTest/src/com/jhapp/main/ServerMain.java
+++ ServerTest/src/com/jhapp/main/ServerMain.java
@@ -1,10 +1,13 @@
 package com.jhapp.main;
 
+import com.jhapp.gui.ServerGui;
+
 public class ServerMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		new ServerGui();
 	}
 
 }
